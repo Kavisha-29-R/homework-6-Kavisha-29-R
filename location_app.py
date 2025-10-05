32ddc05ff1592f30a442762e05d752636a65ffd1
Kavishar29 <1858920@students.wits.ac.za>
2025-10-05 19:15:37 +0200

    add task 2 instructions

---
+++ location_app.py
@@ -0,0 +1,3 @@
+# Task 2:
+#   Create a streamlit app where a user enters longitude and latitude and it creates a map with with a little icon over the spot that you selected. 
+#   Again, post the python code for your app as well as a gif screen capture of it running.
