ed3e0d1575e082b4744f08d900275b5175b9ab17
Kavishar29 <1858920@students.wits.ac.za>
2025-10-05 19:59:44 +0200

    task 3 instructions

---
+++ gdp_app.py
@@ -0,0 +1,5 @@
+# Task 3:
+#       Reuse your python file that webscrapes GDP by country from this link (https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)). 
+#       and plots a stacked interactive bar plot using plotly. Stack countries within regions using the IMF numbers. 
+#       Create streamlit app that displays a stacked bar plot of country GDPs stacked within regions. 
+#       Allow the user to select between the IMF, UN and World Bank reported numbers.
